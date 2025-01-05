0af1d0a35607967bd5c826e313bf4d8217a4b0e4
Yerlan Jumabayev <thevarp18@gmail.com>
2025-01-05 10:57:41 +0500

    fix

--- lms/lms/api.py
+++ lms/lms/api.py
@@ -175,10 +175,7 @@ def get_user_info():
 
 @frappe.whitelist(allow_guest=True)
 def get_translations():
-	if frappe.session.user != "Guest":
-		language = frappe.db.get_value("User", frappe.session.user, "language")
-	else:
-		language = frappe.db.get_single_value("System Settings", "language")
+	language = frappe.db.get_single_value("System Settings", "language")
 	return get_all_translations(language)
 
 
