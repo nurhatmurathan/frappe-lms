68aba9983dd5c2635499f9caecdfac5fee4f4815
Yerlan Jumabayev <thevarp18@gmail.com>
2025-01-06 17:18:26 +0500

    Revert "Reapply "fix""
    This reverts commit 286c7b661d6f7b7aa106a42f4351aa0b56f82866.

--- lms/lms/api.py
+++ lms/lms/api.py
@@ -175,11 +175,8 @@ def get_user_info():
 
 @frappe.whitelist(allow_guest=True)
 def get_translations():
-	if frappe.session.user != "Guest":
-		language = frappe.db.get_value("User", frappe.session.user, "language")
-	else:
-		language = frappe.db.get_single_value("System Settings", "language")
-	return get_all_translations("ru")
+	language = frappe.db.get_single_value("System Settings", "language")
+	return get_all_translations(language)
 
 
 @frappe.whitelist()
