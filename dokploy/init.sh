a8646f43feca34eee0d766d9a9ecb9a389586bfb
nurhatmurathan <nurhatmurathan001@gmail.com>
2025-01-07 18:36:57 +0500

    fix: init force-

--- dokploy/init.sh
+++ dokploy/init.sh
@@ -31,7 +31,6 @@ sed -i '/watch/d' ./Procfile
 bench get-app https://github.com/nurhatmurathan/frappe-lms.git
 
 bench new-site lms.localhost \
---force \
 --mariadb-root-password "${MARIADB_ROOT_PASSWORD:-123}" \
 --admin-password "${ADMIN_PASSWORD:-admin}" \
 --no-mariadb-socket \
