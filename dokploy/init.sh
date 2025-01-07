4a7a850a4ef6fb75699d41cf201b1277f4da2d00
nurhatmurathan <nurhatmurathan001@gmail.com>
2025-01-07 18:04:40 +0500

    fix: init force+

--- dokploy/init.sh
+++ dokploy/init.sh
@@ -31,6 +31,7 @@ sed -i '/watch/d' ./Procfile
 bench get-app https://github.com/nurhatmurathan/frappe-lms.git
 
 bench new-site lms.localhost \
+--force \
 --admin-password "${ADMIN_PASSWORD:-admin}" \
 --no-mariadb-socket \
 --db-name "${DB_NAME:-frappe-lms}" \
